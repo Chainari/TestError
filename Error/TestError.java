411053f27d4b39d9f5288f7b60478c439eed5fac
Wuttha <Wuttha@webmail.npru.ac.th>
2025-06-27 15:52:30 +0700

    first commit

---
+++ Error/TestError.java
@@ -0,0 +1,14 @@
+public class TestError {
+    public static void main(String[] args) {
+        // 1. Compile Error
+        //System.out.println("Hello, World!")
+
+        // 2. Runtime error
+        //System.out.println(5/0); 
+
+        // 3. logical error
+        System.out.println(“0”+”5”+5);
+
+    }
+}
+
